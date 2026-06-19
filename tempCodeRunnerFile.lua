150d9356e6a4dc88e77244e74148f72c3f3ff425
Gregorio Marin <gregm0605@outlook.com>
2026-06-19 16:25:33 -0700

    Added deep.py solution

---
+++ tempCodeRunnerFile.lua
@@ -0,0 +1,5 @@
+answer = input("What is the Answer to the great Question of Life, the Universe, and everything? ")
+if answer == "42", "fourty two:
+    print(yes)
+else 
+    print(no)
---
+++ week 1/deep.py
@@ -0,0 +1,5 @@
+answer = input("What is the Answer to the great Question of Life, the Universe, and everything? ").lower()
+if answer == "42" or answer == "forty two" or answer == "forty-two":
+    print("yes")
+else:
+    print("no")
